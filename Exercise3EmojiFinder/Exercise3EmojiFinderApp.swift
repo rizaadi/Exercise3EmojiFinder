8743c916235d97fadcf8355a1a4781c554069b38
rizaadi <rizaadi890@gmail.com>
2024-02-03 12:40:00 +0700

    Initial Commit

---
+++ Exercise3EmojiFinder/Exercise3EmojiFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise3EmojiFinderApp.swift
+//  Exercise3EmojiFinder
+//
+//  Created by Riza Adi Kurniawan on 03/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Exercise3EmojiFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
